6cb4e5477a9852dc37f63a40e321f1d480b01c59
Chris Butler <chrisb@zedcore.com>
2016-05-10 11:36:52 +0100

    corrected author name

--- src/Collection/Iterator/ClusterKeyspace.php
+++ src/Collection/Iterator/ClusterKeyspace.php
@@ -14,20 +14,20 @@ namespace Predis\Collection\Iterator;
 use Predis\ClientInterface;
 
 /**
- * Abstracts the iteration of the keyspace on a Redis instance by leveraging the
- * SCAN command (Redis >= 2.8) wrapped in a fully-rewindable PHP iterator.
+ * Abstracts the iteration of the keyspace on a Redis 3.0 Cluster by running the
+ * SCAN command across all master nodes in turn.
  *
- * @author Daniele Alessandri <suppakilla@gmail.com>
+ * @author Chris Butler <chrisb@zedcore.com>
  *
  * @link http://redis.io/commands/scan
  */
 class ClusterKeyspace extends CursorBasedIterator
 {
-	/** @var Iterator */
-	private $connections;
+    /** @var Iterator */
+    private $connections;
 
-	/** @var \Predis\ClientInterface */
-	private $currentConnection;
+    /** @var \Predis\ClientInterface */
+    private $currentConnection;
 
     /**
      * {@inheritdoc}
@@ -39,39 +39,39 @@ class ClusterKeyspace extends CursorBasedIterator
         parent::__construct($client, $match, $count);
     }
 
-	protected function reset()
-	{
-		parent::reset();
-		$this->connections = $this->client->getConnection()->getIterator();
-		$this->currentConnection = $this->connections->current();
-	}
+    protected function reset()
+    {
+        parent::reset();
+        $this->connections = $this->client->getConnection()->getIterator();
+        $this->currentConnection = $this->connections->current();
+    }
 
     /**
      * {@inheritdoc}
      */
     protected function executeCommand()
-	{
-		$command = $this->client->createCommand('scan', [max(0, $this->cursor), $this->getScanOptions()]);
-		$result = $this->currentConnection->executeCommand($command);
+    {
+        $command = $this->client->createCommand('scan', [max(0, $this->cursor), $this->getScanOptions()]);
+        $result = $this->currentConnection->executeCommand($command);
 
-		if (!$result[0])
-		{
-			// scan of the current client returned cursor of zero, move onto next connection on the next iteration
-			$this->connections->next();
-			$this->currentConnection = $this->connections->current();
+        if (!$result[0])
+        {
+            // scan of the current client returned cursor of zero, move onto next connection on the next iteration
+            $this->connections->next();
+            $this->currentConnection = $this->connections->current();
 
-			if ($this->currentConnection)
-			{
-				// if this scan returned no entries go for the next connection right now
-				if (count($result[1]) == 0)
-				{
-					return $this->executeCommand();
-				}
+            if ($this->currentConnection)
+            {
+                // if this scan returned no entries go for the next connection right now
+                if (count($result[1]) == 0)
+                {
+                    return $this->executeCommand();
+                }
 
-				$result[0] = -1;
-			}
-		}
+                $result[0] = -1;
+            }
+        }
 
-		return $result;
+        return $result;
     }
 }
